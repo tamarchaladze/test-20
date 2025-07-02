da0041e96b3808d97cf8a6e51627ad8c709dfec0
tamarchaladze <ta.chaladze@gmail.com>
2025-07-03 01:01:40 +0400

    js homework

---
+++ tamar.js
@@ -0,0 +1,57 @@
+a=87
+b=65
+console.log(a+b);
+
+let student = {
+    firstname:"tamar",
+    lastname: "chaladze",
+    age: 37,
+    email:"ta.chaladze@gmail.com",
+    marks: [10, 5, 2]
+
+};
+console.log(student.firstname);
+
+m=45
+n=23
+if (m>n){
+    console.log(m+n)
+}else {
+    console.log(n)
+}
+
+
+let studentname = "tako";
+console.log("rise and shine,tako!")
+
+
+
+let salary=1800;
+let bonus;
+if (salary>=2000){
+    bonus = 0;
+} else {
+    bonus=salary*0.10;
+}
+console.log(bonus)
+
+
+
+a=23
+b=a/2
+c=a+b
+console.log(c)
+
+
+
+phones=["iphone","samsung","devicePixelRatio","huawei","xiaomi"]
+console.log(phones)
+
+
+
+phones=["iphone","samsung","devicePixelRatio","huawei","xiaomi"]
+console.log(phones[0]);
+
+
+phones=["iphone","samsung","devicePixelRatio","huawei","xiaomi"]
+console.log(phones[4])
